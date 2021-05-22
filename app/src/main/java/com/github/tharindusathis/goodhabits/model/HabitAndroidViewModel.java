5b052f5c987ffef2a046529efb28006dbe0691b5
Tharindu Sathischandra <tharindu.sathis@gmail.com>
2021-05-23 00:02:53 +0530

    (refactor) Add habit android view model

---
+++ app/src/main/java/com/github/tharindusathis/goodhabits/model/HabitAndroidViewModel.java
@@ -0,0 +1,45 @@
+package com.github.tharindusathis.goodhabits.model;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.github.tharindusathis.goodhabits.data.HabitDatabase;
+import com.github.tharindusathis.goodhabits.data.HabitRepository;
+
+import java.util.List;
+
+public class HabitAndroidViewModel extends AndroidViewModel {
+
+    public static HabitRepository habitRepository;
+    public final LiveData<List<Habit>> allHabits;
+
+    public HabitAndroidViewModel(@NonNull Application application) {
+        super(application);
+        habitRepository = new HabitRepository(application);
+        allHabits = habitRepository.getAllHabits();
+    }
+
+    public LiveData<List<Habit>> getAllHabits() {
+        return allHabits;
+    }
+
+    public LiveData<Habit> getHabit(long habitId) {
+        return habitRepository.getHabit(habitId);
+    }
+
+    public void insertTask(Habit habit) {
+        habitRepository.insertTask(habit);
+    }
+
+    public void updateTask(Habit habit) {
+        habitRepository.updateTask(habit);
+    }
+
+    public void deleteTask(Habit habit) {
+        habitRepository.deleteTask(habit);
+    }
+
+}
--- app/src/main/java/com/github/tharindusathis/goodhabits/ui/home/HomeFragment.java
+++ app/src/main/java/com/github/tharindusathis/goodhabits/ui/home/HomeFragment.java
@@ -9,13 +9,12 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.github.tharindusathis.goodhabits.R;
-import com.github.tharindusathis.goodhabits.data.HabitRepository;
+import com.github.tharindusathis.goodhabits.model.HabitAndroidViewModel;
 import com.github.tharindusathis.goodhabits.ui.habit.HabitRecyclerViewAdapter;
 
 public class HomeFragment extends Fragment {
@@ -23,6 +22,7 @@ public class HomeFragment extends Fragment {
     private HomeViewModel homeViewModel;
     private RecyclerView habitRecyclerView;
     private HabitRecyclerViewAdapter habitRecyclerViewAdapter;
+    private HabitAndroidViewModel habitAndroidViewModel;
 
     @Override
     public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
@@ -35,18 +35,17 @@ public class HomeFragment extends Fragment {
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         habitRecyclerView = (RecyclerView) root.findViewById(R.id.habit_recycler_view);
         habitRecyclerView.setHasFixedSize(true);
         habitRecyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
 
-        new HabitRepository(requireActivity().getApplication())
+        habitAndroidViewModel = new ViewModelProvider.AndroidViewModelFactory(
+                requireActivity().getApplication()
+        ).create(HabitAndroidViewModel.class);
+
+        habitAndroidViewModel
                 .getAllHabits()
                 .observe(requireActivity(), habits -> {
                             habitRecyclerViewAdapter = new HabitRecyclerViewAdapter(habits);
